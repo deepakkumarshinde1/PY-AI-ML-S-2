11e0de2f30712afb23d241e45efb5f62fd3b2721
deepakkumarshinde1 <deepakkumar.shinde1@gmail.com>
2026-06-05 13:57:16 +0530

    day-2

---
+++ day-2/variables.py
@@ -0,0 +1,25 @@
+variableName = "value"
+variableName1 = "value"
+variableName2 = "value"
+
+_a= "Mouse"
+
+studentName = ""
+student_roll_no = ""
+
+student = "ram"
+studenT = "sham"
+
+print(studenT)
+
+
+productName = productBrand = "Oppo"
+productPrice = 12000
+productModel  = "A5x"
+
+
+productRatting = 4.5
+productIsInStock = False
+
+a , b = 10 , 20
+# b = 20
